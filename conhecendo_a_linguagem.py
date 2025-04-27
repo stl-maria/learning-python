f88d36b63361e894a7a8553562bcad5229ca52bf
stl-maria <emfj.estela@gmail.com>
2025-04-26 21:49:19 -0300

    feat: add initial Python file with first exercise topics

---
+++ conhecendo_a_linguagem.py
@@ -0,0 +1,19 @@
+"""
+01 - Conhecendo a Linguagem Python
+Este arquivo contém uma introdução à linguagem Python com explicações básicas
+e exercícios para prática. Ideal para quem está começando.
+"""
+
+# - Comentários em Python
+# Para uma linha inicie com #
+# Para várias linhas -> """ ou '''
+
+# - Sáida de dados
+print('Olá, mundo')
+
+# - Tipo de dados
+nome = 'Estela'  # str
+idade = 25  # int
+altura = 1.55  # float
+ativo = True  # bool
+
