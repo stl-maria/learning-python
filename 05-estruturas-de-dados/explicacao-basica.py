8a8703f48b35c17cdf9e371e9628946145e6f9af
stl-maria <emfj.estela@gmail.com>
2025-07-18 08:22:08 -0300

    feat: add new file

---
+++ 05-estruturas-de-dados/explicacao-basica.py
@@ -0,0 +1,12 @@
+# criando listas
+frutas = ['laranja', 'maça', 'uva']
+print(frutas)
+
+frutas = []
+print(frutas)
+
+letras = list('python')
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
